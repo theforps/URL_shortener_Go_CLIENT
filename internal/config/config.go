afbd6f5495003f3c4264610bb4a98bc6cfaf78d0
theforps <theforps@gmail.com>
2025-10-02 17:53:24 +0300

    modification of the config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,22 @@
 package config
 
+import "os"
+
 type Config struct {
 	ShortenerURL string
+	TelegramAPI  string
+	Language     string
+}
+
+func NewConfig() *Config {
+	return &Config{
+		ShortenerURL: getEnv("URL_SHORTENER_SERVER"),
+		TelegramAPI:  getEnv("TELEGRAM_BOT_API"),
+		Language:     getEnv("LANGUAGE"),
+	}
+}
+
+func getEnv(key string) (value string) {
+	value, _ = os.LookupEnv(key)
+	return
 }
